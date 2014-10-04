f1d40bd9c4a6fc37542b75a999aa4d66d86a9841
Christian Carnero <christian.carnero@gmail.com>
2014-10-04 18:31:37 -0300

    fix settings

--- samples/shared/Todo/Data/Settings.cs
+++ samples/shared/Todo/Data/Settings.cs
@@ -4,9 +4,9 @@ namespace Todo
 {
 	public class Settings
 	{
-		public static string Marketplace = "https://testssl.kidocloud.com";
-		public static string Application = "tasks";
-		public static string Key = "upTbyOhvjn7/D2jia1D5LkzI66Di4HBoyf4AMFjkZN0=";
+		public static string Marketplace = string.Empty ;
+		public static string Application = string.Empty ;
+		public static string Key = string.Empty ;
 	}
 }
 
